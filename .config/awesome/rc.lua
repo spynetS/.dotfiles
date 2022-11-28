7ee51e8aafb1f9769d8a0d8ede85497f2055f234
spy <alfred@stensatter.se>
2022-11-28 20:45:16 +0100

    u[date

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -168,39 +168,39 @@ local function set_wallpaper(s)
     end
 end
 
-local mpc = require("mpc")
-local textbox = require("wibox.widget.textbox")
-local timer = require("gears.timer")
-local mpd_widget = textbox()
-local state, title, artist, file = "stop", "", "", ""
-local function update_widget()
-    local text = "Current MPD status: "
-    text = text .. tostring(artist or "") .. " - " .. tostring(title or "")
-    if state == "pause" then
-        text = text .. " (paused)"
-    end
-    if state == "stop" then
-        text = text .. " (stopped)"
-    end
-    mpd_widget.text = text
-end
-local connection
-local function error_handler(err)
-    mpd_widget:set_text("Error: " .. tostring(err))
-    -- Try a reconnect soon-ish
-    timer.start_new(10, function()
-        connection:send("ping")
-    end)
-end
-connection = mpc.new(nil, nil, nil, error_handler,
-    "status", function(_, result)
-        state = result.state
-    end,
-    "currentsong", function(_, result)
-        title, artist, file = result.title, result.artist, result.file
-        pcall(update_widget)
-    end)
-mpd_widget:buttons(awful.button({}, 1, function() connection:toggle_play() end))
+--local mpc = require("mpc")
+--local textbox = require("wibox.widget.textbox")
+--local timer = require("gears.timer")
+--local mpd_widget = textbox()
+--local state, title, artist, file = "stop", "", "", ""
+--local function update_widget()
+--    local text = "Current MPD status: "
+--    text = text .. tostring(artist or "") .. " - " .. tostring(title or "")
+--    if state == "pause" then
+--        text = text .. " (paused)"
+--    end
+--    if state == "stop" then
+--        text = text .. " (stopped)"
+--    end
+--    mpd_widget.text = text
+--end
+--local connection
+--local function error_handler(err)
+--    mpd_widget:set_text("Error: " .. tostring(err))
+--    -- Try a reconnect soon-ish
+--    timer.start_new(10, function()
+--        connection:send("ping")
+--    end)
+--end
+--connection = mpc.new(nil, nil, nil, error_handler,
+--    "status", function(_, result)
+--        state = result.state
+--    end,
+--    "currentsong", function(_, result)
+--        title, artist, file = result.title, result.artist, result.file
+--        pcall(update_widget)
+--    end)
+--mpd_widget:buttons(awful.button({}, 1, function() connection:toggle_play() end))
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
 
