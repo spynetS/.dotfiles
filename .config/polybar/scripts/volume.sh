c3d15f745fddd1a43018baa0264e08239a312f5f
spy <alfred@stensatter.se>
2023-02-19 00:13:29 +0100

    update to volume script

--- .config/polybar/scripts/volume.sh
+++ .config/polybar/scripts/volume.sh
@@ -8,23 +8,29 @@ then
         volume=`pactl get-sink-volume 66 | awk '{print substr($5, 1, length($5)-1)}'`
         id=`pactl list short sinks | grep RUNNING | awk '{print $1}'`
 
-        if [ `pactl get-sink-mute $id | awk '{print $2}'` = 'yes' ]
+        if [ ! -z $id ]
         then
-            echo 'mute'
-        else
-            echo $volume
+            if [ `pactl get-sink-mute $id | awk '{print $2}'` = 'yes' ]
+            then
+                echo 'mute'
+            else
+                echo $volume
+            fi
         fi
         sleep 0.1
     done
 else
     id=`pactl list short sinks | grep RUNNING | awk '{print $1}'`
-    if [ "$1" = 'mute' ]
+    if [ ! -z $id ]
     then
-    if [ `pactl get-sink-mute $id | awk '{print $2}'` = 'no' ]
-    then
-        pactl set-sink-mute $id yes
-    else
-        pactl set-sink-mute $id no
+        if [ "$1" = 'mute' ]
+        then
+        if [ `pactl get-sink-mute $id | awk '{print $2}'` = 'no' ]
+        then
+            pactl set-sink-mute $id yes
+        else
+            pactl set-sink-mute $id no
+        fi
     fi
 
 fi
